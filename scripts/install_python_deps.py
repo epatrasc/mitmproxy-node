247083f6c4a64288fb29af10a729e0426262aa85
Emilian Patrasc <emilian.patrasc@bbc.co.uk>
2018-08-08 15:21:25 +0100

    remove mitmproxy version checker

--- scripts/install_python_deps.py
+++ scripts/install_python_deps.py
@@ -9,19 +9,6 @@ from subprocess import check_call, check_output
 if sys.version_info < (3,6):
     sys.exit("mitmproxy-node requires a Python version >= 3.6, but found {}.{}".format(sys.version_info[0], sys.version_info[1]))
 
-# Verify that mitmproxy is installed
-try:
-    mitmdump_output = str(check_output(["mitmdump", "--version"]))
-    version = mitmdump_output.split("\\n")[0].split(" ")[1].split('.')
-    print(version)
-    version[0] = int(version[0])
-    version[1] = int(version[1])
-    version[2] = int(version[2])
-    if tuple(version) < (4,0,0):
-        sys.exit("mitmproxy-node requires mitmproxy >= 4.0.0, but found {}.{}.{}".format(version[0], version[1], version[2]))
-except FileNotFoundError:
-    sys.exit("mitmproxy-node requires mitmproxy to be installed. See http://docs.mitmproxy.org/en/stable/install.html for instructions.")
-
 # Install dependencies with pip3 first.
 # If pip3 isn't found, use pip.
 try:
